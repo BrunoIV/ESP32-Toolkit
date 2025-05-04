84485b16308d78074f8f34a3fa5ba8272d145d5f
bexposito <bruno.exposito@gmail.com>
2025-05-04 20:49:36 +0200

    Add default SSID/PASS

--- include/secrets.h
+++ include/secrets.h
@@ -1,8 +1,8 @@
 #ifndef SECRETS_H
 #define SECRETS_H
 
-const char* WIFI_SSID = "";
-const char* WIFI_PASS = "";
+const char* WIFI_SSID = "Esp32";
+const char* WIFI_PASS = "EspAdmin_32";
 
 #endif
 
