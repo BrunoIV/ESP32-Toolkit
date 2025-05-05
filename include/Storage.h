acaab6944a462009966c9fb8e379f2f830875dde
bexposito <bruno.exposito@gmail.com>
2025-05-05 18:51:26 +0200

    Modified Storage class for allow list a different folder than /

--- include/Storage.h
+++ include/Storage.h
@@ -3,11 +3,12 @@
 
 #include <Arduino.h>
 #include <vector>
+#include "MenuItem.h"
 
 class Storage {
 public:
     static void init();
-    static std::vector<String> listDir();
+    static std::vector<MenuItem> listDir(String path);
     static void writeFile(String path, String content);
     static boolean deleteFile(String path);
     static String readFile(String path);
--- src/Storage.cpp
+++ src/Storage.cpp
@@ -1,4 +1,5 @@
 #include "Storage.h"
+#include "MenuItem.h"
 #include "SPIFFS.h"
 
 #define FORMAT_SPIFFS_IF_FAILED true
@@ -10,9 +11,9 @@ void Storage::init() {
     }
 }
 
-std::vector<String> Storage::listDir() {
+std::vector<MenuItem> Storage::listDir(String folder) {
     File root = SPIFFS.open("/");
-    std::vector<String> files;
+    std::vector<MenuItem> files;
 
     if (!root) {
         Serial.println("Error getting the list of files");
@@ -20,9 +21,38 @@ std::vector<String> Storage::listDir() {
     }
 
     File file = root.openNextFile();
+
     while (file) {
-        files.push_back(String(file.path()));
-        file = root.openNextFile();
+      String filePath = String(file.path());
+
+      if(filePath.startsWith(folder)) {
+
+        String filePathWithoutFolder = filePath.substring(folder.length());
+        bool isFile = filePathWithoutFolder.indexOf("/") <= 0;
+        filePathWithoutFolder = filePathWithoutFolder.substring(0, filePathWithoutFolder.indexOf("/"));
+
+        if(isFile) {
+          MenuItem menu = MenuItem("", "/edit?file=" + String(file.path()), file.name(), "fa fa-file", std::vector<MenuItem>());
+          files.push_back(menu);
+        } else {
+
+          bool exists = false;
+          for (const MenuItem& item : files) {
+              if (item.getName() == filePathWithoutFolder) {
+                  exists = true;
+                  break;
+              }
+          }
+
+          if(!exists) {
+            MenuItem menu = MenuItem("", "/files?folder=" + folder + filePathWithoutFolder + "/", filePathWithoutFolder, "fa fa-folder", std::vector<MenuItem>());
+            files.push_back(menu);
+          }
+       
+        }
+      }
+
+      file = root.openNextFile();
     }
 
     return files;
