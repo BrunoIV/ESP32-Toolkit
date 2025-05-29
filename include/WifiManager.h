d872abe389cd3ef0722640c6dc100af673514ffd
bexposito <bruno.exposito@gmail.com>
2025-05-29 15:53:33 +0200

    Moved getNetworks to WifiManager

--- include/WifiManager.h
+++ include/WifiManager.h
@@ -1,10 +1,13 @@
 #pragma once
 #include <Arduino.h>
+#include <vector>
+#include "WifiNetwork.h"
 
 class WifiManager {
     public:
         static void connect(String ssid, String password);
         static void init();
+        static std::vector<WifiNetwork> getNetworks();
 
     private:
         
--- src/WifiManager.cpp
+++ src/WifiManager.cpp
@@ -1,12 +1,13 @@
 #include "WifiManager.h"
 #include <WiFi.h>
+#include <vector>
 #include "secrets.h"
+#include "WifiNetwork.h"
 
 void WifiManager::init() {
   WiFi.mode(WIFI_AP_STA);
   WiFi.softAP(WIFI_SSID, WIFI_PASS);
 }
-
 
 void WifiManager::connect(const String ssid, const String password) {
   Serial.print("Connecting to " + ssid);
@@ -22,4 +23,20 @@ void WifiManager::connect(const String ssid, const String password) {
   Serial.println(WiFi.localIP());
 
   delay(200);
+}
+
+
+std::vector<WifiNetwork> WifiManager::getNetworks() {
+  std::vector<WifiNetwork> networks;
+
+  Serial.println("Scanning networks...");
+  delay(100);
+
+  int size = WiFi.scanNetworks();
+  const String encriptationsName[10] = { "OPEN", "WEP", "WPA PSK", "WPA2 PSK", "WPA WPA2 PSK", "WPA2 ENTERPRISE", "WPA3 PSK", "WPA2 WPA3 PSK", "WAPI_PSK", "MAX" };
+  for (int i = 0; i < size; ++i) {
+    networks.push_back(WifiNetwork(WiFi.SSID(i), WiFi.channel(i), WiFi.BSSID(i), encriptationsName[WiFi.encryptionType(i)], WiFi.RSSI(i)));
+  }
+
+  return networks;
 }
