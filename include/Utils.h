29abd6ad81c87b3d157965269e4d94fc7ce759bd
BrunoIV <bruno.exposito@gmail.com>
2026-05-01 10:42:39 +0200

    Add ip conversion helpers

--- include/Utils.h
+++ include/Utils.h
@@ -9,6 +9,9 @@ public:
     static std::vector<String> splitString(const String &str, char delimiter);
     static String escapeHTML(String input);
     static String randomString(int length);
+    static bool isValidIPv4(const String& ip);
+    static uint32_t ipToInt(int a, int b, int c, int d);
+    static void intToIp(uint32_t ip, int &a, int &b, int &c, int &d);
 };
 
 #endif
--- src/Utils.cpp
+++ src/Utils.cpp
@@ -38,3 +38,29 @@ String Utils::randomString(int length) {
 
   return result;
 }
+
+bool Utils::isValidIPv4(const String& ip) {
+  int a, b, c, d;
+
+  if (sscanf(ip.c_str(), "%d.%d.%d.%d", &a, &b, &c, &d) != 4)
+    return false;
+
+  return (a >= 0 && a <= 255 &&
+          b >= 0 && b <= 255 &&
+          c >= 0 && c <= 255 &&
+          d >= 0 && d <= 255);
+}
+
+uint32_t Utils::ipToInt(int a, int b, int c, int d) {
+  return ((uint32_t)a << 24) |
+         ((uint32_t)b << 16) |
+         ((uint32_t)c << 8)  |
+         (uint32_t)d;
+}
+
+void Utils::intToIp(uint32_t ip, int &a, int &b, int &c, int &d) {
+  a = (ip >> 24) & 0xFF;
+  b = (ip >> 16) & 0xFF;
+  c = (ip >> 8) & 0xFF;
+  d = ip & 0xFF;
+}
