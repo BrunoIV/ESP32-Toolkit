2f25880c594f679d047cec90f127c82a23bc5479
BrunoIV <bruno.exposito@gmail.com>
2026-05-02 09:22:33 +0200

    Add icon sprite generator script

---
+++ scripts/sprite_generator.py
@@ -0,0 +1,34 @@
+Import("env")
+import os
+
+input_dir = "images"
+output_file = "data/templates/cmn/icons.svg"
+
+with open(output_file, "w") as out:
+    out.write('<svg style="display:none;">\n')
+
+    for filename in os.listdir(input_dir):
+        if not filename.endswith(".svg"):
+            continue
+
+        filepath = os.path.join(input_dir, filename)
+
+        name = os.path.splitext(filename)[0]
+
+        out.write(f'<symbol id="{name}" viewBox="0 -960 960 960">\n')
+
+        with open(filepath, "r") as f:
+            content = f.read()
+
+            content = content.replace("<path", "\n<path")
+
+            content = content.replace("</svg", "\n")
+
+            lines = content.splitlines()
+            for line in lines:
+                if "<path" in line:
+                    out.write(line.strip() + "\n")
+
+        out.write('</symbol>\n')
+
+    out.write('</svg>\n')
